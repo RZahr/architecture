ff64d9aee107aea6599b77eeda8f60f22524f2b3
RZahr <dev-mobile@geovision.com.lb>
2020-11-14 22:31:17 +0200

    initial

--- architecture/src/main/java/com/rzahr/architecture/ActivityExtensions.kt
+++ architecture/src/main/java/com/rzahr/architecture/ActivityExtensions.kt
@@ -1,8 +1,12 @@
 package com.rzahr.architecture
 
+import android.content.Intent
+import android.content.pm.PackageManager
 import android.content.res.Configuration
+import android.net.Uri
 import android.os.Build
 import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.recyclerview.widget.DividerItemDecoration
@@ -41,4 +45,19 @@ fun AppCompatActivity.setToolbar(toolbar: Toolbar, title: String, displayHomeAsU
     setSupportActionBar(toolbar)
 
     supportActionBar?.setDisplayHomeAsUpEnabled(displayHomeAsUpEnabled)
+}
+
+
+/**
+ * opens google map application if available
+ * @param uri: the uri of the application
+ */
+fun AppCompatActivity.openGoogleMapsApp(uri: String) {
+
+    val intent = Intent(Intent.ACTION_VIEW, Uri.parse(uri))
+    intent.setClassName("com.google.android.apps.maps", "com.google.android.maps.MapsActivity")
+
+    if (packageManager.queryIntentActivities(intent, PackageManager.MATCH_DEFAULT_ONLY).size > 0)  this.startActivity(intent)
+
+    else Toast.makeText(this.applicationContext, "Please Install Google Navigation", Toast.LENGTH_LONG).show()
 }
