b053c3bd08d3b28c2a31d13955077484a088b592
RZahr <dev-mobile@geovision.com.lb>
2020-11-17 21:54:43 +0200

    initial

---
+++ architecture/src/main/java/com/rzahr/architecture/clean_architecture/UseCase.kt
@@ -0,0 +1,35 @@
+package com.rzahr.architecture.clean_architecture
+
+abstract class UseCase<Q : UseCase.RequestValues, P : UseCase.ResponseValue> {
+
+    var requestValues: Q? = null
+
+    var useCaseCallback: UseCaseCallback<P>? = null
+
+    internal fun run() {
+        execute(requestValues)
+    }
+
+    internal fun destroy() {
+        finishUseCase()
+    }
+
+    protected abstract fun finishUseCase()
+
+    protected abstract fun execute(requestValues: Q?)
+
+    /**
+     * Data passed to a request.
+     */
+    interface RequestValues
+
+    /**
+     * Data received from a request.
+     */
+    interface ResponseValue
+
+    interface UseCaseCallback<R> {
+        fun onSuccess(response: R)
+        fun onError(t: Throwable)
+    }
+}
---
+++ architecture/src/main/java/com/rzahr/architecture/clean_architecture/UseCaseHandler.kt
@@ -0,0 +1,42 @@
+package com.rzahr.architecture.clean_architecture
+
+class UseCaseHandler() {
+
+    fun <T : UseCase.RequestValues, R : UseCase.ResponseValue> execute(useCase: UseCase<T, R>, values: T, callback: UseCase.UseCaseCallback<R>) {
+
+        useCase.requestValues = values
+        useCase.useCaseCallback = UiCallbackWrapper(callback, this)
+        useCase.run()
+    }
+
+    private fun <V : UseCase.ResponseValue> notifyResponse(response: V, useCaseCallback: UseCase.UseCaseCallback<V>) {
+        useCaseCallback.onSuccess(response)
+    }
+
+    private fun <V : UseCase.ResponseValue> notifyError(useCaseCallback: UseCase.UseCaseCallback<V>, t: Throwable) {
+        useCaseCallback.onError(t)
+    }
+
+    private class UiCallbackWrapper<V : UseCase.ResponseValue>(private val mCallback: UseCase.UseCaseCallback<V>, private val mUseCaseHandler: UseCaseHandler) : UseCase.UseCaseCallback<V> {
+
+        override fun onSuccess(response: V) {
+            mUseCaseHandler.notifyResponse(response, mCallback)
+        }
+
+
+        override fun onError(t: Throwable) {
+            mUseCaseHandler.notifyError(mCallback, t)
+        }
+    }
+
+    companion object {
+
+        private var INSTANCE: UseCaseHandler? = null
+
+        fun getInstance(): UseCaseHandler {
+            if (INSTANCE == null) INSTANCE = UseCaseHandler()
+
+            return INSTANCE!!
+        }
+    }
+}
