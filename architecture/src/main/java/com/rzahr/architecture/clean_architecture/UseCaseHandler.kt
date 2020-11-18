aa8958016a6e77f48938ad805be372c8fd46c438
RZahr <dev-mobile@geovision.com.lb>
2020-11-18 13:58:31 +0200

    initial

--- architecture/src/main/java/com/rzahr/architecture/clean_architecture/UseCaseHandler.kt
+++ architecture/src/main/java/com/rzahr/architecture/clean_architecture/UseCaseHandler.kt
@@ -1,5 +1,8 @@
 package com.rzahr.architecture.clean_architecture
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+
 class UseCaseHandler() {
 
     fun <T : UseCase.RequestValues, R : UseCase.ResponseValue> execute(useCase: UseCase<T, R>, values: T, callback: UseCase.UseCaseCallback<R>) {
@@ -7,6 +10,13 @@ class UseCaseHandler() {
         useCase.requestValues = values
         useCase.useCaseCallback = UiCallbackWrapper(callback, this)
         useCase.run()
+    }
+
+    fun <T : UseCase.RequestValues, R : UseCase.ResponseValue> executeN(UseCaseN: UseCase<T, R>, values: T, liveData: LiveData<LiveDataState<R>>) {
+
+        UseCaseN.requestValues = values
+        UseCaseN.useCaseCallback = UiCallbackWrapperN(liveData)
+        UseCaseN.run()
     }
 
     fun <T : UseCase.RequestValues, R : UseCase.ResponseValue> finish(useCase: UseCase<T, R>) {
@@ -19,6 +29,19 @@ class UseCaseHandler() {
 
     private fun <V : UseCase.ResponseValue> notifyError(useCaseCallback: UseCase.UseCaseCallback<V>, t: Throwable) {
         useCaseCallback.onError(t)
+    }
+
+    private class UiCallbackWrapperN<V : UseCase.ResponseValue>(private val liveData: LiveData<LiveDataState<V>>) : UseCase.UseCaseCallback<V> {
+
+        override fun onSuccess(response: V) {
+            liveData as MutableLiveData
+            liveData.value = LiveDataState.Success(response)
+        }
+
+        override fun onError(t: Throwable) {
+            liveData as MutableLiveData
+            liveData.value = LiveDataState.Failure(t)
+        }
     }
 
     private class UiCallbackWrapper<V : UseCase.ResponseValue>(private val mCallback: UseCase.UseCaseCallback<V>, private val mUseCaseHandler: UseCaseHandler) : UseCase.UseCaseCallback<V> {
