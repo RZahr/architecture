bd081cc9e53ec3561e0a410e39f5669d87100692
RZahr <dev-mobile@geovision.com.lb>
2020-11-26 11:28:53 +0200

    initial

--- architecture/src/main/java/com/rzahr/architecture/clean_architecture/UseCase.kt
+++ architecture/src/main/java/com/rzahr/architecture/clean_architecture/UseCase.kt
@@ -1,5 +1,6 @@
 package com.rzahr.architecture.clean_architecture
 
+
 abstract class UseCase<Q : UseCase.RequestValues, P : UseCase.ResponseValue> {
 
     //var requestValues: Q? = null
@@ -8,7 +9,7 @@ abstract class UseCase<Q : UseCase.RequestValues, P : UseCase.ResponseValue> {
     var useCaseCallback: UseCaseCallback<P>? = null
     var useCaseCallbackN: UseCaseCallbackN<P>? = null
 
-    internal fun run() {
+    internal suspend fun run() {
         execute(requestValues)
     }
 
@@ -18,17 +19,12 @@ abstract class UseCase<Q : UseCase.RequestValues, P : UseCase.ResponseValue> {
 
     protected abstract fun clearUseCase()
 
-    protected abstract fun execute(requestValues: Q)
-  //  protected abstract fun execute(requestValues: Q?)
+    protected abstract suspend fun execute(requestValues: Q)
+    //  protected abstract fun execute(requestValues: Q?)
 
-    /**
-     * Data passed to a request.
-     */
+
     interface RequestValues
 
-    /**
-     * Data received from a request.
-     */
     interface ResponseValue
 
     interface UseCaseCallback<R> {
@@ -43,3 +39,49 @@ abstract class UseCase<Q : UseCase.RequestValues, P : UseCase.ResponseValue> {
         fun onLoading()
     }
 }
+
+
+//backup without coroutines
+// abstract class UseCase<Q : UseCase.RequestValues, P : UseCase.ResponseValue> {
+//
+//    //var requestValues: Q? = null
+//    lateinit var requestValues: Q
+//
+//    var useCaseCallback: UseCaseCallback<P>? = null
+//    var useCaseCallbackN: UseCaseCallbackN<P>? = null
+//
+//    internal fun run() {
+//        execute(requestValues)
+//    }
+//
+//    internal fun onClear() {
+//        clearUseCase()
+//    }
+//
+//    protected abstract fun clearUseCase()
+//
+//    protected abstract fun execute(requestValues: Q)
+//  //  protected abstract fun execute(requestValues: Q?)
+//
+//    /**
+//     * Data passed to a request.
+//     */
+//    interface RequestValues
+//
+//    /**
+//     * Data received from a request.
+//     */
+//    interface ResponseValue
+//
+//    interface UseCaseCallback<R> {
+//        fun onSuccess(response: R)
+//        fun onError(t: Throwable)
+//        fun onLoading()
+//    }
+//
+//    interface UseCaseCallbackN<R> {
+//        fun onSuccess(response: R)
+//        fun onError(t: Throwable)
+//        fun onLoading()
+//    }
+//}
--- architecture/src/main/java/com/rzahr/architecture/clean_architecture/UseCaseHandler.kt
+++ architecture/src/main/java/com/rzahr/architecture/clean_architecture/UseCaseHandler.kt
@@ -1,47 +1,31 @@
+@file:Suppress("unused")
+
 package com.rzahr.architecture.clean_architecture
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+
+//https://www.toptal.com/android/android-apps-mvvm-with-clean-architecture
+
 
 class UseCaseHandler() {
 
-    fun <T : UseCase.RequestValues, R : UseCase.ResponseValue> execute(useCase: UseCase<T, R>, values: T, callback: UseCase.UseCaseCallback<R>) {
+    suspend fun <T : UseCase.RequestValues, R : UseCase.ResponseValue> execute(useCase: UseCase<T, R>, values: T, callback: UseCase.UseCaseCallback<R>) {
 
         useCase.requestValues = values
         useCase.useCaseCallback = UiCallbackWrapper(callback, this)
-        useCase.useCaseCallback?.onLoading()
+        withContext(Dispatchers.Main) { useCase.useCaseCallback?.onLoading() }
         useCase.run()
     }
-
-
-    /*example
-    *
-    * useCaseHandler.execute(getBundles, GetBundlesUseCase.RequestValues(),
-                object : UseCase.UseCaseCallback<GetBundlesUseCase.ResponseValue> {
-
-                    override fun onSuccess(response: GetBundlesUseCase.ResponseValue) {
-                        bundlesLiveData as MutableLiveData
-                        bundlesLiveData.value = response.bundles
-                    }
-
-                    override fun onError(t: Throwable) {
-                        bundlesLiveData as MutableLiveData
-                        bundlesLiveData.value = null
-                    }
-                })
-    * */
-
-    fun <T : UseCase.RequestValues, R : UseCase.ResponseValue> executeWithLiveData(UseCaseN: UseCase<T, R>, values: T, liveData: LiveData<LiveDataState<R>>) {
+    suspend fun <T : UseCase.RequestValues, R : UseCase.ResponseValue> executeWithLiveData(UseCaseN: UseCase<T, R>, values: T, liveData: LiveData<LiveDataState<R>>) {
 
         UseCaseN.requestValues = values
         UseCaseN.useCaseCallbackN = UiCallbackWrapperN(liveData)
-        UseCaseN.useCaseCallbackN?.onLoading()
+        withContext(Dispatchers.Main) { UseCaseN.useCaseCallbackN?.onLoading() }
         UseCaseN.run()
     }
-    /*example
-    *
-    *  useCaseHandler.executeWithLiveData(getMessages, requestValue, messagesLiveData)
-    * */
 
     fun <T : UseCase.RequestValues, R : UseCase.ResponseValue> finish(useCase: UseCase<T, R>) {
         useCase.onClear()
@@ -103,3 +87,106 @@ class UseCaseHandler() {
         }
     }
 }
+
+
+//backup without coroutines
+//class UseCaseHandler() {
+//
+//    fun <T : UseCase.RequestValues, R : UseCase.ResponseValue> execute(useCase: UseCase<T, R>, values: T, callback: UseCase.UseCaseCallback<R>) {
+//
+//        useCase.requestValues = values
+//        useCase.useCaseCallback = UiCallbackWrapper(callback, this)
+//        useCase.useCaseCallback?.onLoading()
+//        useCase.run()
+//    }
+//
+//
+//    /*example
+//    *
+//    * useCaseHandler.execute(getBundles, GetBundlesUseCase.RequestValues(),
+//                object : UseCase.UseCaseCallback<GetBundlesUseCase.ResponseValue> {
+//
+//                    override fun onSuccess(response: GetBundlesUseCase.ResponseValue) {
+//                        bundlesLiveData as MutableLiveData
+//                        bundlesLiveData.value = response.bundles
+//                    }
+//
+//                    override fun onError(t: Throwable) {
+//                        bundlesLiveData as MutableLiveData
+//                        bundlesLiveData.value = null
+//                    }
+//                })
+//    * */
+//
+//    fun <T : UseCase.RequestValues, R : UseCase.ResponseValue> executeWithLiveData(UseCaseN: UseCase<T, R>, values: T, liveData: LiveData<LiveDataState<R>>) {
+//
+//        UseCaseN.requestValues = values
+//        UseCaseN.useCaseCallbackN = UiCallbackWrapperN(liveData)
+//        UseCaseN.useCaseCallbackN?.onLoading()
+//        UseCaseN.run()
+//    }
+//    /*example
+//    *
+//    *  useCaseHandler.executeWithLiveData(getMessages, requestValue, messagesLiveData)
+//    * */
+//
+//    fun <T : UseCase.RequestValues, R : UseCase.ResponseValue> finish(useCase: UseCase<T, R>) {
+//        useCase.onClear()
+//    }
+//
+//    private fun <V : UseCase.ResponseValue> notifyResponse(response: V, useCaseCallback: UseCase.UseCaseCallback<V>) {
+//        useCaseCallback.onSuccess(response)
+//    }
+//
+//    private fun <V : UseCase.ResponseValue> notifyError(useCaseCallback: UseCase.UseCaseCallback<V>, t: Throwable) {
+//        useCaseCallback.onError(t)
+//    }
+//
+//    private fun <V : UseCase.ResponseValue> notifyLoading(useCaseCallback: UseCase.UseCaseCallback<V>) {
+//        useCaseCallback.onLoading()
+//    }
+//
+//    private class UiCallbackWrapperN<V : UseCase.ResponseValue>(private val liveData: LiveData<LiveDataState<V>>) : UseCase.UseCaseCallbackN<V> {
+//
+//        override fun onSuccess(response: V) {
+//            liveData as MutableLiveData
+//            liveData.value = LiveDataState.Success(response)
+//        }
+//
+//        override fun onError(t: Throwable) {
+//            liveData as MutableLiveData
+//            liveData.value = LiveDataState.Failure(t)
+//        }
+//
+//        override fun onLoading() {
+//            liveData as MutableLiveData
+//            liveData.value = LiveDataState.Loading
+//        }
+//    }
+//
+//    private class UiCallbackWrapper<V : UseCase.ResponseValue>(private val mCallback: UseCase.UseCaseCallback<V>, private val mUseCaseHandler: UseCaseHandler) : UseCase.UseCaseCallback<V> {
+//
+//        override fun onSuccess(response: V) {
+//            mUseCaseHandler.notifyResponse(response, mCallback)
+//        }
+//
+//        override fun onError(t: Throwable) {
+//            mUseCaseHandler.notifyError(mCallback, t)
+//        }
+//
+//        override fun onLoading() {
+//            mUseCaseHandler.notifyLoading(mCallback)
+//        }
+//    }
+//
+//    companion object {
+//
+//        private var INSTANCE: UseCaseHandler? = null
+//
+//        fun getInstance(): UseCaseHandler {
+//            if (INSTANCE == null) INSTANCE = UseCaseHandler()
+//
+//            return INSTANCE!!
+//        }
+//    }
+//}
